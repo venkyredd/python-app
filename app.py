44c1a26021f5d9bff22fee8d542d7dad569131fb
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-03-21 16:35:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def get_data():
     return jsonify({"message": "This is data from Flask API", "status": "success"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
