4cd99d481118439ac23b224da15b748d3e01155c
Ubuntu <ubuntu@ip-172-31-12-195.ap-south-1.compute.internal>
2025-03-21 05:48:31 +0000

    Better Flask App with API and Templates

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', title="My Flask App")
+
+@app.route('/api/data')
+def get_data():
+    return jsonify({"message": "This is data from Flask API", "status": "success"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
